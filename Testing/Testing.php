c183abf74a0e73add8dc2decf2e042977ef59e79
Mohammed Saleh <mohammed.saleh.22707@gmail.com>
2026-03-14 04:13:20 +0200

    refactor Testing: simplify query operations, remove unused logic, and re-enable debug print methods

--- Testing/Testing.php
+++ Testing/Testing.php
@@ -37,22 +37,10 @@ $withs = [
     "cta2" => $sub
 ];
 
-$t->select("table")->where($where)
-    ->order(["col1", "col2"=> "asc", "{$t->subQuery()->select('suboo')->where(["sqlo", "=", "valuo"])}" => "desc", "col4"])
-    ->withSQL($withs)
-    ->select("sorry")
-    ->rawSQL("Testing for RAW SQL")
-    ->insert("table", ["id", "username", "passsowtrd"])
-    ->values([14, "user", "pasopaso"])
-    ->values([16, "usero", "pasopaso"])
-    ->insert("halo", ["h", "w"])
-    ->select("helo", ["h", "w"])->where(["h", "in", "{1, 2, 3, 4, 5}"])
-    ->update("ta", ["col"=>"val", "colo"=>12])
-    ->where(["id", "!=", 4])
-    ->delete("t")
-    ->where(["id", "<", 60])
+$t->select("table")->where(["table.col", ">", 34])
     ->callDB()
 ;
-//print_r($t->getParams());
-//print_r($t->getQuery());
-//print_r($t->getQueries());
+
+print_r($t->getParams());
+print_r($t->getQuery());
+print_r($t->getQueries());
